a09e09104ce473f80e5d5279a852c18eefeff6cc
Naoya0827 <n.freedom.yazuna@gmail.com>
2021-10-30 09:17:29 +0000

    [Fix]4-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,19 @@
 class BooksController < ApplicationController
-  def index
-  end
 
-  def new
-   @book = Book.new
-  end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
