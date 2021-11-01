8b45fd6d6c02c2abb23c878ab1b0ac63277b000b
Naoya0827 <n.freedom.yazuna@gmail.com>
2021-11-01 10:58:29 +0000

    [Add-bari]4-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book)
+      flash[:success] = "Book was successfully created"
     else
       @books = Book.all
       render :index
@@ -25,9 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :edit
+    end
   end
 
   def destroy
